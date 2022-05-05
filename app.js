beeb38b9b5ec482809c04f86e1aee025a5399cc8
snexcolly123 <paruj.lab@gmail.com>
2022-05-05 13:25:54 +0700

    basic cmoplete

--- app.js
+++ app.js
@@ -69,18 +69,66 @@ app.post("/todos", async (req, res, next) => {
   }
 });
 
+//Update Todo: PUT/todos/:id
+//BODY: title(required), completed(default:false), dueDate
+app.put("/todos/:id", async (req, res, next) => {
+  try {
+    const params = req.params;
+    const { title, completed = false, dueDate = null } = req.body;
+    // console.log(req.body.title)
+    // console.log(typeof(req.body.title))
+    if (typeof title !== "string") {
+        console.log(title)
+      //เขียนreturn แทน else เพื่อบังคับจบการทำงาน โค้ดด้านล่างได้เลย
+      //การส่งresponeออกไปไม่ได้เเปลว่ามันจะหยุดการทำงาน
+      createError("title must be a string", 400);
+    }
+
+    if (validator.isEmpty(title)) {
+      createError("Title is require.", 400);
+    }
+
+    if (typeof completed !== "boolean") {
+      createError("Completed must be a boolean.", 400);
+    }
+    if (dueDate !== null && !validator.isDate(dueDate + "")) {
+      //และ ไม่ใช่รูปแบบวันที่
+      createError("dueDate must be a date string.", 400);
+    }
+    const todos = await readTodos();
+    const idx = todos.findIndex((el) => el.id === params.id);
+    if (idx === -1) {
+      createError("todo is not found", 400);
+    }
+
+    console.log(req.body)
+
+    todos[idx] = {
+      id: params.id,
+      title,
+      completed,
+      dueDate: dueDate === null ? dueDate : new Date(dueDate),
+    };
+
+    await writeTodos(todos);
+    res.json({ todo: todos[idx] });
+  } catch (err) {
+    next(err);
+  }
+});
+
 //DELETE tdo:DELETE/todos/:id
 app.delete("/todos/:id", async (req, res, next) => {
   try {
     const params = req.params;
     const todos = await readTodos();
-    const idx = todos.findIndex(el => el.id === params.id)
-    if(idx === -1){
-        createError('todo is not found',400)
-    }todos.splice(idx,1)
-    await writeTodos(todos)
+    const idx = todos.findIndex((el) => el.id === params.id);
+    if (idx === -1) {
+      createError("todo is not found", 400);
+    }
+    todos.splice(idx, 1);
+    await writeTodos(todos);
     res.status(204).json();
-
   } catch (err) {
     next(err);
   }
